0a47234aed9ebd46831d167a3d8ec9609c4c0a99
JakubKozak <jakub.kozak15@seznam.cz>
2020-10-06 13:13:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function Chello() {
+  let jm = dokument.getElementById("jmeno").value;
+  console.log(jm);
+  alert("reee")
+}
+
+
+function vypocet() {
+  let c1 = dokument.getElementById("cislo1").value;
+  let c2 = dokument.getElementById("cislo2").value;
+  let vysl = c1 + c2
+  console.log(vysl);
+}
